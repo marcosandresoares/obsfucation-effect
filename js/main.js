67159dc0bcbec154c2aff8adefa40a2046a8b4dd
Marcos Soares <31077979+marcosandresoares@users.noreply.github.com>
2019-07-01 18:11:55 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const text = baffle('.data');
+text.set({
+    characters: 'zpxzVpasdfh86136░█▒ ░░░█▓ >░░ ▓/▒█▓ █ █>█▒sayg▒ ░░░█▓ >yf',
+    speed: 150
+});
+text.start();
+text.reveal(70000);
+
+const quote = baffle('.quote');
+quote.set({
+    characters: '⨈⫳ªէµ℗ϗ⫯∂?⫯κ∏º⫝$⫳⫯♱ѕяο⨃₲ℏ∀⫭է∑яⴤօ∪∫₤⫯ძ⫝₶⋿ητ₶ℇ⊂⦿ρ$⊂',
+    speed: 140
+});
+quote.start();
+quote.reveal(5500);
